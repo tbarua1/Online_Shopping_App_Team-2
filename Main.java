017aac29a3dead1a2b56bb608cb1ff81ec66c768
Fahima <fahima.ansari@scorpius.com.my>
2021-06-17 13:08:16 +0530

    1 file uploaded

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args ) {
+	// write your code here
+        short a,b,c;
+        a=1;
+        b=2;
+        c= (short) (a+b);
+        System.out.println(c);
+    }
+}
